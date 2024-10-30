a9a410782b4007b66850c6ffd993ecf106245561
unk217 <unk00@hotmail.com>
2024-10-30 18:44:59 -0500

    simple auth token

--- auth/views.py
+++ auth/views.py
@@ -1,13 +1,25 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .serializer import UserSerializer
-from rest_framework.authtoken.models import Token
+from rest_framework.authtoken.models import Token  
 from django.contrib.auth.models import User
+from rest_framework import status
+from django.shortcuts import get_object_or_404
+from rest_framework.decorators import authentication_classes, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.authentication import TokenAuthentication
 
 @api_view(['POST'])
 def login(request):
+    user = get_object_or_404(User, username=request.data['username'])
     
-    return Response({})
+    if not user.check_password(request.data['password']):
+        return Response({"error":"Invalid password"}, status=status.HTTP_400_BAD_REQUEST)
+    
+    token, created = Token.objects.get_or_create(user=user)
+    serializer = UserSerializer(instance=user)
+    
+    return Response({"token": token.key, "user": serializer.data}, status=status.HTTP_200_OK)
 
 @api_view(['POST'])
 def register(request):
@@ -19,8 +31,17 @@ def register(request):
         user.set_password(serializer.data['password'])
         user.save()
         
-    return Response({})
+        token = Token.objects.create(user=user)
+        return Response({'token': token.key, "user": serializer.data}, status=status.HTTP_201_CREATED)
+        
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 @api_view(['POST'])
+@authentication_classes([TokenAuthentication])
+@permission_classes([IsAuthenticated])
 def profile(request):
-    return Response({})
+    
+    print(request.user)
+    serializer = UserSerializer(instance=request.user)
+    
+    return Response(serializer.data, status=status.HTTP_200_OK)
